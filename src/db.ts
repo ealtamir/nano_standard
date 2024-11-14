556dd9275caec947761a8e2b3215910b4c2e2e27
Enzo Altamiranda <enzo.altamiranda@deeppsy.io>
2024-11-14 07:50:32 +0100

    Updated prod password details.

--- src/db.ts
+++ src/db.ts
@@ -5,17 +5,17 @@ const ENVIRONMENT = Deno.env.get("ENVIRONMENT") || "development";
 const config = ENVIRONMENT === "production" 
   ? {
       // Production database configuration
-      host: "localhost",
-      database: "nano_xperiments",
-      username: "nano_xperiments",
-      password: Deno.env.get("POSTGRES_PASSWORD"),
+      host: Deno.env.get("POSTGRES_HOST"),
+      database: Deno.env.get("POSTGRES_DATABASE"),
+      username: Deno.env.get("POSTGRES_USER"),
+      password: Deno.env.get("DB_USER_PASSWORD"),
     }
   : {
       // Development database configuration
       host: "localhost",
       database: "postgres",
       username: "postgres",
-      password: "your_password",
+      password: "password",
     };
 
 const sql = postgres(config) // will use psql environment variables
