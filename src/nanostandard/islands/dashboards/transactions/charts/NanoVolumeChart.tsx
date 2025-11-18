4f668bc0e54984ef8fef6436af1e14e29ee58602
Enzo Altamiranda <enzo.altamiranda@deeppsy.io>
2025-11-17 22:07:31 -0300

    Included the mean in the volumes chart and changed its colors.

--- src/nanostandard/islands/dashboards/transactions/charts/NanoVolumeChart.tsx
+++ src/nanostandard/islands/dashboards/transactions/charts/NanoVolumeChart.tsx
@@ -122,7 +122,7 @@ export default function NanoVolumeChart() {
           mode: "lines+markers",
           yaxis: "y1",
           line: {
-            color: "#4299E1",
+            color: "#22C55E",
             width: 2,
           },
           marker: {
@@ -138,7 +138,23 @@ export default function NanoVolumeChart() {
           mode: "lines+markers",
           yaxis: "y1",
           line: {
-            color: "#48BB78",
+            color: "#F59E0B",
+            width: 2,
+          },
+          marker: {
+            size: 4,
+            symbol: "circle",
+          },
+        },
+        {
+          x: chartData.map((d) => new Date(d.time_bucket)),
+          y: chartData.map((d) => chartRound(d.rolling_average)),
+          name: `Rolling Average [${viewType2MedianRange(viewType)}]`,
+          type: "scatter",
+          mode: "lines+markers",
+          yaxis: "y1",
+          line: {
+            color: "#8B5CF6",
             width: 2,
           },
           marker: {
